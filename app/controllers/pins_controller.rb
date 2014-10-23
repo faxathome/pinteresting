c76e705983cd5d72c5cf60f96e7cb561d941376d
faxathome <parafajardo@gmail.com>
2014-10-23 20:25:04 +0200

    Add Amazon S3 for Paperclip upload to heroku

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,8 +1,8 @@
 class PinsController < ApplicationController
   respond_to :html, :xml, :json 
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
   before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -53,6 +53,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description, :image)
+      params.require(:pin).permit(:description)
     end
 end
