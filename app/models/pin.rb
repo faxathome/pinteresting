9e7d87ef8f5d9bbdbff53402e0f5264a37103651
faxathome <parafajardo@gmail.com>
2014-10-28 23:46:58 +0100

    Fixed Paperclip error

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,7 @@
 class Pin < ActiveRecord::Base
 			belongs_to :user
 
-			has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/Faxi.png"
-			validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+			has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://www.invertersale.com/media/product_medium/165_image-not-found.gif"
+			# validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+			do_not_validate_attachment_file_type :image
 end
