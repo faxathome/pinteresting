b6c31d43257179334b672e5fbd5cfef58a8e757c
faxathome <parafajardo@gmail.com>
2014-10-30 17:32:18 +0100

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,7 @@ class Pin < ActiveRecord::Base
 			has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://www.invertersale.com/media/product_medium/165_image-not-found.gif"
 			# validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 			do_not_validate_attachment_file_type :image
+
+			validates :image, presence: true
+			validates :description, presence: true
 end
